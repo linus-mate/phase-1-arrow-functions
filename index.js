56c40e42cf49967c709345b82cee4195c78107bc
linus mutugi mate <mlynux@gmail.com>
2022-08-17 10:51:46 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+
+//const divide = (num1, num2) => num1/num2;
+  //divide(2000, 100);
+
+  const divide = function() {
+    return 2000/100;
+  };
+  
+ 
+
+  const square = (x) => x * 2;
+  square(2);
+
+
+  const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(3,4);
